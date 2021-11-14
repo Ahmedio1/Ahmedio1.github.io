68ff26f2f80aa97af7afddbf8b2b176ec7d7385d
Ahmedio1 <ahmed__daoud@outlook.fr>
2021-11-14 18:41:22 +0100

    Debut site

---
+++ portfolio/resources/views/index.blade.php
@@ -0,0 +1,33 @@
+@extends('master')
+@section('title', 'Portfolio')
+@section('main')
+
+
+    <div class="container fluid">
+    <div class="row pt-5 pb-5">
+        <div class="col-4">
+        </div>
+        <div class="col mb-5">
+            <h1>Ahmed Daoud</h1>
+            <p>Etudiant en 2eme année de DUT informatique</p>
+            <div class="row">
+                <div class="col-3">
+                    <button type="button" class="btn btn-outline-dark">A propos</button>
+                </div>
+                <div class="col">
+                    <button type="button" class="btn btn-outline-dark">Compétences</button>
+                </div>
+                <div class="col pb-5">
+                    <button type="button" class="btn btn-outline-dark">Projets</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-3">
+        </div>
+    </div>
+    </div>
+@endsection
+
+
+
