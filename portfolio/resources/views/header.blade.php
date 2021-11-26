ba56f0cd7d5d8944f51550453196630acc27301c
Ahmedio1 <ahmed__daoud@outlook.fr>
2021-11-27 00:15:34 +0100

    "Finition"

--- portfolio/resources/views/header.blade.php
+++ portfolio/resources/views/header.blade.php
@@ -1,23 +1,22 @@
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
     <div class="container" id="header">
-            <a class="navbar-brand" href="#">Daoud Ahmed</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active px-5" aria-current="page" href="#">A propos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active px-5" href="#">Mes compétences</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active px-5" aria-current="page" href="#">Projets</a>
-                    </li>
-                </ul>
-            </div>
+        <a class="navbar-brand" href="#">Daoud Ahmed</a>
+
+        <ul class="navbar-nav mr-auto" id="aligner">
+            <li class="nav-item">
+                <a class="nav-link active " aria-current="page" href="#">A propos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active " href="#">Mes compétences</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active " aria-current="page" href="#">Formation</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active " aria-current="page" href="#">Projets</a>
+            </li>
+        </ul>
+
     </div>
-    </nav>
+</nav>
 
